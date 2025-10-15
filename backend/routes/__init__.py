66014a566a23c5bc0cc969e6c50e67b7a47f3751
Nothing... <adamrifandy123@gmail.com>
2025-10-15 14:59:19 +0700

    Added __init__.py and Updated bills.py

---
+++ backend/routes/__init__.py
@@ -0,0 +1,1 @@
+from .bills import bills_bp
--- backend/routes/bills.py
+++ backend/routes/bills.py
@@ -1,9 +1,9 @@
 from flask import Blueprint, jsonify
 from services.db import get_db_connection
 
-add_bill_bp = Blueprint("bills_bp", __name__)
+bills_bp = Blueprint("bills_bp", __name__)
 
-@add_bill_bp.route("/bills", methods=["GET"])
+@bills_bp.route("/bills", methods=["GET"])
 def bills():
     connection = get_db_connection()
     cursor = connection.cursor(dictionary=True)
