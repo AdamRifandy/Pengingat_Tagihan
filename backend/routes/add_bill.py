3e370183202137b9472f7418b699890395bb6a00
Nothing... <adamrifandy123@gmail.com>
2025-10-15 14:44:30 +0700

    Added add_bill.py

---
+++ backend/routes/add_bill.py
@@ -0,0 +1,14 @@
+from flask import Blueprint, request, jsonify
+from services.db import get_db_connection
+
+add_bill = Blueprint("add_bill", __name__)
+
+@add_bill.route("/add_bill", methods=["POST"])
+def add_bill():
+    data = request.get_json()
+    
+    connection = get_db_connection()
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO all_bills VALUES (%s)", ())
+    return jsonify({"message": "Data berhasil ditambahkan."}), 201
+    
