81d72201ea4a1c07b9d5e9d821475b4749724538
Nothing... <adamrifandy123@gmail.com>
2025-10-15 15:01:46 +0700

    Updated edit_bill.py

--- backend/routes/edit_bill.py
+++ backend/routes/edit_bill.py
@@ -1,9 +1,9 @@
 from flask import Blueprint, request, jsonify
 from services.db import get_db_connection
 
-edit_bill = Blueprint("edit_bill", __name__)
+edit_bill_bp = Blueprint("edit_bill", __name__)
 
-@edit_bill.route("/edit_bill/<int:id>", methods=["PUT"])
+@edit_bill_bp.route("/edit_bill/<int:id>", methods=["PUT"])
 def edit_bill(id):
     data = request.get_json
     
