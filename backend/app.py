c8a33fbca6aca09e4c00f201bdde19a4d2537038
Nothing... <adamrifandy123@gmail.com>
2025-10-15 15:12:08 +0700

    Registered all routes

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,13 @@
 from flask import Flask
 from flask_cors import CORS
+from routes import bills_bp, add_bill_bp, edit_bill_bp, remove_bill_bp
 
 app = Flask(__name__)
 CORS(app)
+
+app.register_blueprint(bills_bp)
+app.register_blueprint(add_bill_bp)
+app.register_blueprint(edit_bill_bp)
+app.register_blueprint(remove_bill_bp)
 
 app.run(debug=True)
